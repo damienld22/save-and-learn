57c200568c464e086f0263f8710e36a864644d47
Damien Le Dantec <damien-le-dantec@hotmail.fr>
2018-09-09 11:55:04 +0200

    feature(front) - Delete best practices

--- front/src/components/BestPractices.vue
+++ front/src/components/BestPractices.vue
@@ -10,7 +10,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-bind:key="bp.id" v-for="bp in bestPractices">
+        <tr v-bind:key="index" v-for="(bp, index) in bestPractices">
           <td class="bpTitle">{{ bp.title }}</td>
           <td class="bpContent">{{ bp.content }}</td>
           <td class="editBtn">
@@ -22,7 +22,7 @@
             </a>
           </td>
           <td class="deleteBtn">
-            <a class="button is-danger is-outlined">
+            <a class="button is-danger is-outlined" @click="deleteBestPractice(index)">
               <span>Delete</span>
               <span class="icon is-small">
                 <i class="fas fa-times"></i>
@@ -32,18 +32,27 @@
         </tr>
       </tbody>
     </table>
+    <simplert :useRadius="true"
+      :useIcon="true"
+      ref="simplert">
+    </simplert>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
 import { URL_SERVER_BEST_PRACTICES } from '../utils/network'
+import Simplert from 'vue2-simplert'
 
 export default {
   name: 'BestPractices',
+  components: {
+    Simplert
+  },
   data: () => {
     return {
-      bestPractices: null
+      bestPractices: null,
+      indexSelectedBestPractice: null
     }
   },
   created: function () {
@@ -54,6 +63,28 @@ export default {
       const url = URL_SERVER_BEST_PRACTICES
       axios.get(url).then((result) => {
         this.bestPractices = result.data['_embedded']['best-practice']
+      }).catch((err) => {
+        // TODO: Handle error
+        console.error(err)
+      })
+    },
+    deleteBestPractice: function (index) {
+      this.indexSelectedBestPractice = index
+      let deleteModal = {
+        title: 'Do you want to delete this best practice ?',
+        message: `Your best practice is : "${this.bestPractices[index].title}"`,
+        type: 'warning',
+        useConfirmBtn: true,
+        customConfirmBtnText: 'Yes',
+        onConfirm: this.onDeleteModalConfirm
+      }
+      this.$refs.simplert.openSimplert(deleteModal)
+    },
+    onDeleteModalConfirm: function () {
+      const linkOfBestPractice = this.bestPractices[this.indexSelectedBestPractice]['_links']['self']['href']
+      axios.delete(linkOfBestPractice).then((res) => {
+        this.bestPractices.splice(this.indexSelectedBestPractice, 1)
+        this.indexSelectedBestPractice = null
       }).catch((err) => {
         // TODO: Handle error
         console.error(err)
---
+++ front/src/components/DeleteModal.vue
@@ -0,0 +1,7 @@
+<script>
+import Simplert from 'vue2-simplert'
+
+export default {
+  
+}
+</script>
